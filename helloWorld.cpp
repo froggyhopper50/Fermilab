4ab0e51d4a7afd34b37bb0177b9103eb914ba731
James Kennedy <40674318+froggyhopper50@users.noreply.github.com>
2018-07-02 16:07:12 -0500

    Update helloWorld.cpp

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 int main()
 {
+	//Displays to user:
+	//Hello World!
+	//Your robot overlords are here!
+	
 	cout << "Hello World!" << endl;
 	cout << "Your robot overlords are here!" << endl;
 
