d2a955dd0a65a33bb6d4e6fefae66f0f17f835f6
James Kennedy <40674318+froggyhopper50@users.noreply.github.com>
2018-07-02 16:09:03 -0500

    Added lines

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -7,9 +7,11 @@ int main()
 	//Displays to user:
 	//Hello World!
 	//Your robot overlords are here!
+	//All your bases are belong to us!
 	
 	cout << "Hello World!" << endl;
 	cout << "Your robot overlords are here!" << endl;
+	cout << "All your bases are belong to us!" << endl;
 
 	return 0;
 }
